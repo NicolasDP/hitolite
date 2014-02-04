2c98f405a149866499dfcfdb231a8a14681b0c26
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-02-04 19:00:22 +0000

    minor improvment

--- Hitolite/Hitolite.hs
+++ Hitolite/Hitolite.hs
@@ -1,7 +1,14 @@
 import System.Environment
 import System.Process
 
+findIn :: String -> [(String, String)] -> Maybe String
+findIn _   []         = Nothing
+findIn key ((k,c):xs) = if (key == k) then Just c else findIn key xs
+
 main = do
-  args <- getArgs
-  originalCmd <- getEnv "SSH_ORIGINAL_COMMAND"
-  system originalCmd
+    userName <- getArgs
+    envs <- getEnvironment
+    case findIn "SSH_ORIGINAL_COMMAND" envs of
+        Nothing   -> putStrLn "error, command not found"
+        Just ocmd -> do returnCode <- system ocmd
+                        putStrLn $ show returnCode
